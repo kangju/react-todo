121b59ca347e93fed367e91857b7ae4c324fa3b1
kangju <kangju0815@gmail.com>
2020-07-27 08:42:06 +0000

    TODOLISTを別ファンクション化

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,6 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+
+function TodoItem(props){
+    return (
+        <li key={props.index}>
+            <div className="todoitem">
+                <div>
+                    {props.value}
+                </div>
+                <div>
+                    <button onClick={props.onRemoveButtonClick}>削除</button>
+                </div>
+            </div>
+        </li>
+    );
+}
+
+function TodoList(props){
+    const todos = props.todos;
+    const todoItems = todos.map((value,index)=>{
+        return (
+            <TodoItem value={value} index={index} onRemoveButtonClick={() => props.onRemoveButtonClick(index)}/>
+        );
+    });
+    return (
+        <div class="todolist">
+            <h2>TODO List</h2>
+            <ol>{todoItems}</ol>
+        </div>
+    );
+}
+
 class Todo extends React.Component
 {
     constructor(props){
@@ -19,7 +50,15 @@ class Todo extends React.Component
         });
     }
 
+    onInputFieldKeyPress(e){
+        if(e.which === 13) this.addTodoItem();
+    }
+
     onAddButtonClick(e){
+        this.addTodoItem();
+    }
+
+    addTodoItem(){
         const inputtext = this.state.inputtext.slice();
         if(!inputtext)
             return;
@@ -30,26 +69,29 @@ class Todo extends React.Component
         });
     }
 
+    onRemoveButtonClick(index)
+    {
+        const todos = this.state.todos.slice();
+        todos.splice(index,1);
+        this.setState({
+            todos:todos
+        });
+    }
+
     render(){
         const todos = this.state.todos.slice();
-        const todolist = todos.map((value,index)=>{
-            return (
-                <li key="{index}">
-                    {value}
-                </li>
-            );
-        });
         return (
             <div class="todo">
                 <div class="inputfield">
                     <h2>Input</h2>
-                    <input type="text" value={this.state.inputtext} onChange={(e) => this.onInputFieldChange(e)} placeholder="TODO"/>
+                    <input type="text"
+                        value={this.state.inputtext}
+                        onChange={(e) => this.onInputFieldChange(e)}
+                        onKeyPress={(e) => this.onInputFieldKeyPress(e)}
+                        placeholder="TODO"/>
                     <button onClick={(e) => this.onAddButtonClick(e)}>登録</button>
                 </div>
-                <div class="todolist">
-                    <h2>TODO List</h2>
-                    <ol>{todolist}</ol>
-                </div>
+                <TodoList todos={todos} onRemoveButtonClick={(index) => this.onRemoveButtonClick(index)} />
             </div>
         );
     }
