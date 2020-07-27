22a80c9247591c6516f0ea2ea3c35911d3378bdd
kangju <kangju0815@gmail.com>
2020-07-27 07:19:47 +0000

    TODO追加処理作成

---
+++ my-app/src/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Todo extends React.Component
+{
+    constructor(props){
+        super(props);
+        this.state = {
+            todos:[],
+            inputtext:"",
+        }
+
+//        this.onAddButtonClick = this.onAddButtonClick.bind(this);
+//        this.onInputFieldChange = this.onInputFieldChange.bind(this);
+    }
+
+    onInputFieldChange(e){
+        const inputValue = e.target.value.slice();
+        this.setState({
+            inputtext:inputValue,
+        });
+    }
+
+    onAddButtonClick(e){
+        const inputtext = this.state.inputtext.slice();
+        if(!inputtext)
+            return;
+
+        this.setState({
+            todos:this.state.todos.concat(inputtext),
+            inputtext:"",
+        });
+    }
+
+    render(){
+        const todos = this.state.todos.slice();
+        const todolist = todos.map((value,index)=>{
+            return (
+                <li key="{index}">
+                    {value}
+                </li>
+            );
+        });
+        return (
+            <div class="todo">
+                <div class="inputfield">
+                    <h2>Input</h2>
+                    <input type="text" value={this.state.inputtext} onChange={(e) => this.onInputFieldChange(e)} placeholder="TODO"/>
+                    <button onClick={(e) => this.onAddButtonClick(e)}>登録</button>
+                </div>
+                <div class="todolist">
+                    <h2>TODO List</h2>
+                    <ol>{todolist}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Todo />,document.getElementById("root"));
